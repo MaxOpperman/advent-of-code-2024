2a55d1e8a92c20ddfce2e8983a32584f57a91da6
Max <m.opperman98@gmail.com>
2024-12-05 11:03:11 +0100

    Linting

--- puzzles/day5/main.py
+++ puzzles/day5/main.py
@@ -6,7 +6,9 @@ def read_file(input_file: str) -> list[str]:
         return [line.strip() for line in f]
 
 
-def five_a(page_order = list[tuple[int, int]], updates = list[str]) -> tuple[int, list[int]]:
+def five_a(
+    page_order=list[tuple[int, int]], updates=list[str]
+) -> tuple[int, list[int]]:
     correct_updates, incorrect_updates = [], []
     for u in updates:
         update_pages = [int(num) for num in u.split(",")]
@@ -24,12 +26,14 @@ def five_a(page_order = list[tuple[int, int]], updates = list[str]) -> tuple[int
             incorrect_updates.append(update_pages)
     res = 0
     for u in correct_updates:
-        res += int(u[int((len(u) - 1)/2)])
+        res += int(u[int((len(u) - 1) / 2)])
     print(f"Five A result: {res}")
     return res, incorrect_updates
 
 
-def check_order(page_order = list[tuple[int, int]], updates = list[int]) -> tuple[bool, int]:
+def check_order(
+    page_order=list[tuple[int, int]], updates=list[int]
+) -> tuple[bool, int]:
     seen = []
     for i, update_page in enumerate(updates):
         for page in page_order:
@@ -39,7 +43,7 @@ def check_order(page_order = list[tuple[int, int]], updates = list[int]) -> tupl
     return True, -1
 
 
-def five_b(page_order = list[tuple[int, int]], updates = list[str]) -> int:
+def five_b(page_order=list[tuple[int, int]], updates=list[str]) -> int:
     new_correct_updates = []
     for u in tqdm(updates):
         update_pages = [int(num) for num in u.split(",")]
@@ -54,8 +58,13 @@ def five_b(page_order = list[tuple[int, int]], updates = list[str]) -> int:
             if page[0] in update_pages or page[1] in update_pages:
                 current_page_rules.append(page)
         while not correct_order:
-            update_pages[abs(ind - swap_i)], update_pages[ind] = update_pages[ind], update_pages[abs(ind - swap_i)]
-            partial_correct_order, ind = check_order(current_page_rules, update_pages[:ind])
+            update_pages[abs(ind - swap_i)], update_pages[ind] = (
+                update_pages[ind],
+                update_pages[abs(ind - swap_i)],
+            )
+            partial_correct_order, ind = check_order(
+                current_page_rules, update_pages[:ind]
+            )
             if not partial_correct_order:
                 swap_i += 1
                 continue
@@ -65,16 +74,14 @@ def five_b(page_order = list[tuple[int, int]], updates = list[str]) -> int:
         new_correct_updates.append(update_pages)
     res = 0
     for u in new_correct_updates:
-        res += int(u[int((len(u) - 1)/2)])
+        res += int(u[int((len(u) - 1) / 2)])
     print(f"Five B result: {res}")
     return res
-            
 
 
 if "__main__" == __name__:
     text = read_file("./inputs/day5.txt")
-    page_order, updates = text[:text.index("")], text[text.index("") + 1:]
+    page_order, updates = text[: text.index("")], text[text.index("") + 1 :]
     page_order = [tuple([int(page) for page in line.split("|")]) for line in page_order]
     five_a(page_order, updates)
     five_b(page_order, updates)
-
